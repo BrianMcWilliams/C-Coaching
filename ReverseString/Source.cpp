a58b0c76cd3f6e1d7a160b88e71971c7757c1010
Brian <brianmcwilliams15@gmail.com>
2021-02-16 09:46:03 -0500

    Reverse String Solution

---
+++ ReverseString/Source.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+
+using namespace std;
+
+const char* originalString = "Racecar is a palindrome";
+
+void ReverseString(char* stringToReverse)
+{
+
+
+	char* stringEnd = stringToReverse;
+	char tempChar = 0;
+
+	while (*(stringEnd + sizeof(char)) != 0)
+	{
+		stringEnd++;
+	}
+
+	while (stringEnd != stringToReverse && stringEnd > stringToReverse)
+	{
+		tempChar = *stringToReverse;
+		*stringToReverse = *stringEnd;
+		*stringEnd = tempChar;
+
+		stringEnd--;
+		stringToReverse++;
+	}
+}
+
+int main(void)
+{
+	void* memory = malloc(0x50);
+
+	assert(memory != 0);
+	
+	strcpy_s((char*)memory, sizeof(char) * 50 , originalString);
+	char* strPtr = (char*)memory;
+
+	cout << "Let's reverse this string : " << (char*)memory << endl;
+
+	ReverseString(strPtr);
+	
+	cout << "Here's the reversed string : " << (char*)memory << endl;
+	system("pause");
+}
